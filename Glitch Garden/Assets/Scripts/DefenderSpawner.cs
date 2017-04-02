e71fc6df7ba23611a85e40ffef61d74ebdf3a71c
Michael Young <mvy1221@gmail.com>
2017-04-02 16:29:41 -0700

    167

---
+++ Glitch Garden/Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+
+    public Camera myCamera;
+
+    private GameObject defenderParent;
+
+    private void Start()
+    {
+        defenderParent = GameObject.Find("Defenders");
+
+        if (!defenderParent)
+        {
+            defenderParent = new GameObject("Defenders");
+        }
+    }
+    private void OnMouseDown()
+    {
+        print(Input.mousePosition);
+        print(SnapToGrid (CalculateWorldPointOfMouseClick ()));
+
+        Vector2 rawPos = CalculateWorldPointOfMouseClick();
+        Vector2 roundedPos = SnapToGrid(rawPos);
+        GameObject defender = Button.currentlySelected;
+
+        GameObject newDef = Instantiate(defender, roundedPos, Quaternion.identity) as GameObject;
+        newDef.transform.parent = defenderParent.transform;
+    }
+
+    Vector2 SnapToGrid(Vector2 rawWorldPosition)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPosition.x);
+        float newY = Mathf.RoundToInt(rawWorldPosition.y);
+
+        return new Vector2(newX, newY);
+    }
+
+    Vector2 CalculateWorldPointOfMouseClick()
+    {
+        float mouseX = Input.mousePosition.x;
+        float mouseY = Input.mousePosition.y;
+        float distanceFromCamera = 10f;
+
+        Vector3 weirdTriplet = new Vector3(mouseX, mouseY, distanceFromCamera);
+        Vector2 worldPos = myCamera.ScreenToWorldPoint(weirdTriplet);
+
+        return worldPos;
+    }
+}
